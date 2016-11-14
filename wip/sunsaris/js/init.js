5f1ffbaa24aca512e4509e6b2abee7e6f23a718e
codebooker <codebooker@gmail.com>
2016-11-14 13:11:11 -0500

    Changed Init

--- wip/sunsaris/js/init.js
+++ wip/sunsaris/js/init.js
@@ -1,7 +1,7 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
