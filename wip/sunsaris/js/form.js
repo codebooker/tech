708f149b885865fcae349cf1e67031f27f14f0c6
codebooker <codebooker@gmail.com>
2016-11-14 00:38:25 -0500

    Sunsaris update

--- wip/sunsaris/js/form.js
+++ wip/sunsaris/js/form.js
@@ -1,35 +1,12 @@
-$('.carousel').carousel({
-    full_width: true
-    , time_constant: 100
-    , indicators: false
+$(document).ready(function () {
+    $('select').material_select();
 });
-$('.next').click(function () {
-    $('.carousel').carousel('next');
+$('.submit').click(function () {
+    Materialize.toast('We Are Calculating Your Savings', 3000, 'rounded') // 'rounded' is the class I'm applying to the toast
+    setTimeout(function () {
+        $('ul.tabs').tabs('select_tab', 'test12');
+    }, 1500); // How long do you want the delay to be (in milliseconds)? 
 });
 $('.prev').click(function () {
     $('.carousel').carousel('prev');
 });
-$('.startbutton').click(function () {
-    $('.carousel').carousel('next');
-});
-// Materialize.toast(message, displayLength, className, completeCallback);
-Materialize.toast('', 4000) // 4000 is the duration of the toast
-$(document).ready(function () {
-    $('select').material_select();
-});
-var $select = $('select');
-
-$select
-    .on('change', function () {
-        var $this = $(this),
-            // use replace to remove extra white (if desired)
-            txt = $this.find('option:selected').text().replace(/\s+/g, ' ');
-        // add title to select
-        $this.attr('title', txt);
-    })
-    .change()
-    .find('option').each(function () {
-        var $this = $(this);
-        // add title to each option, so it works on hover
-        $this.attr('title', $this.text());
-    });
